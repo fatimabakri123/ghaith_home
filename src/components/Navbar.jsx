5faaece9a5a9cf53f8cd84ba62a4aa3345501823
Fatima baki <101220702@mu.edu.lb>
2026-09-22 19:16:04 +0300

    Update website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useLanguage } from "../context/LanguageContext";
 import { useBridalList } from "../context/BridalListContext";
-
+import "./navbar.css";
 function Navbar() {
   const { list } = useBridalList();
   const { language, toggleLanguage, t } = useLanguage();
